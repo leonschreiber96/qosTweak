f723bb45169a329048daae560753c54c62612b9a
Leon Schreiber <leon.schreiber@tu-berlin.de>
2024-10-24 16:19:39 +0200

    Add model library

---
+++ shared-model/index.ts
@@ -0,0 +1,1 @@
+export * from "./webRtcCommand.js";
---
+++ shared-model/webRtcCommand.ts
@@ -0,0 +1,12 @@
+enum WebRtcCommand {
+   WantToCall = "want-to-call",
+   OtherIsCalling = "other-is-calling",
+   TakeCall = "take-call",
+   CallBegins = "call-begins",
+   InitiateCall = "initiate-call",
+   CallPeer = "call-peer",
+   AnswerMade = "answer-made",
+   HangUp = "hang-up",
+}
+
+export default WebRtcCommand;
